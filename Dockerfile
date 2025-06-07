0b40342b4730f6fb5911ae347c744c456af66f1e
jccaldw1 <jacobccaldwell@outlook.com>
2025-06-07 15:48:41 -0700

    add some logs

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Start with an official Go image
+FROM golang:1.21-alpine
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy go.mod and go.sum and download dependencies
+COPY go.mod ./
+RUN go mod download
+
+# Copy the source code
+COPY . .
+
+# Build the Go binary
+RUN go build -o myapp .
+
+# Expose port 8080
+EXPOSE 8080
+
+# Run the binary
+CMD ["./myapp"]
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 
@@ -9,7 +10,12 @@ import (
 )
 
 func main() {
+	fmt.Println("hello world")
 	connStr := os.Getenv("CONNECTION_STRING")
+	if connStr == "" {
+		log.Fatal("Could not open connection")
+		return
+	}
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal("Could not open db connection: ", err)
